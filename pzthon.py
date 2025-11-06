6743a7afc31b03f3a4e7cbd1dd37dc28ff54729d
unknown <hadmin@pcfeib207-77.msad.vsb.cz>
2025-11-06 17:02:38 +0100

    skoro hotovo

--- pzthon.py
+++ pzthon.py
@@ -6,35 +6,32 @@ while loop == 1:
     oprt=input("operace: ").replace(" ", "")
     num2=input("druhe cislo: ").replace(" ", "")
 
-    if not num1.isdigit() or not num2.isdigit():
-        if num1.isalpha() or num2.isalpha():
-            print("DEMENTALE / písmena nejsou čísla")
-        else:    
-            print("DEMENTALE / operatory nejsou čísla")
+    if num1=="π" or num1=="pi":
+        num1=3.1415926535
     else:
+        num1=float(num1)
+    if num2=="π" or num2=="pi":
+        num2=3.1415926535
+    else:
+        num2=float(num2)
+    if num1=="exit" or num2=="exit" or oprt=="exit":
+        break
 
-    # TVUJ KOD:
-        if  num1.isnumeric() or num2.isnumeric():
-            if num1=="" or num2=="":                         
-                print("Napiš tam nějaké číslo!")
-            elif oprt == "+" or  oprt == "-" or  oprt == "*" or  oprt == "/":
-                if oprt=="+":
-                    print(int(num1) + int(num2))
-                elif oprt=="-":
-                    print(int(num1) - int(num2))
-                elif oprt=="*":
-                    print(int(num1) * int(num2))
-                elif oprt=="/":
-                    if int(num2)==0:
-                        print("Jsi hloupý 0 nelze dělit!!!")
-                    else:
-                        print(int(num1) / int(num2))
-                else:
-                    print("Tato operace neexistuje!!")
-            else:
-                print("Nesprávný operator.")
-        else:   
-            if num1=="" or num2=="": 
-                print("Napiš tam nějaké číslo!")
-            else:   
-                print("Toto je kalkulačka, písmena bys zadávat neměl!")
+# TVUJ KOD:
+    #if  num1.isnumeric() or num2.isnumeric():
+    if oprt=="+":
+        print(int(num1) + int(num2))
+    elif oprt=="-":
+        print(int(num1) - int(num2))
+    elif oprt=="*":
+        #print(int(num1) * int(num2))
+        print(num1*num2)
+    elif oprt=="/":
+        if int(num2)==0:
+            print("Jsi hloupý 0 nelze dělit!!!")
+        else:
+            print(int(num1) / int(num2))
+    else:
+        print("Tato operace neexistuje!!")
+    # else:   
+    #     print("Syntax ERROR")
