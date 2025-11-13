ea5db586f8a2fffc09fe29799c2a7cbaa10a705d
unknown <hadmin@pcfeib207-77.msad.vsb.cz>
2025-11-13 16:50:59 +0100

    Konecne hotovo

--- pzthon.py
+++ pzthon.py
@@ -1,37 +1,47 @@
+
 #  NEUPRAVOVAT
 loop = 1
 while loop == 1:
+
+    vysledek=0
 
     num1=input("prvni cislo: ").replace(" ", "")
     oprt=input("operace: ").replace(" ", "")
     num2=input("druhe cislo: ").replace(" ", "")
 
-    if num1=="π" or num1=="pi":
-        num1=3.1415926535
-    else:
-        num1=float(num1)
-    if num2=="π" or num2=="pi":
-        num2=3.1415926535
-    else:
-        num2=float(num2)
-    if num1=="exit" or num2=="exit" or oprt=="exit":
-        break
+
+    try:
+
+        if num1=="π" or num1=="pi":
+            num1=3.1415926535
+        else:
+            num1=float(num1)
+        if num2=="π" or num2=="pi":
+            num2=3.1415926535
+        else:
+            num2=float(num2)
+
 
 # TVUJ KOD:
-    #if  num1.isnumeric() or num2.isnumeric():
-    if oprt=="+":
-        print(int(num1) + int(num2))
-    elif oprt=="-":
-        print(int(num1) - int(num2))
-    elif oprt=="*":
-        #print(int(num1) * int(num2))
-        print(num1*num2)
-    elif oprt=="/":
-        if int(num2)==0:
-            print("Jsi hloupý 0 nelze dělit!!!")
+
+        if oprt=="+":
+            vysledek=num1 + num2
+        elif oprt=="-":
+            vysledek=num1 - num2
+        elif oprt=="*":
+            vysledek=num1*num2
+        elif oprt=="/":
+            if int(num2)==0:
+                print("Jsi hloupý 0 nelze dělit!!!")
+            else:
+                vysledek=num1 / num2
         else:
-            print(int(num1) / int(num2))
-    else:
-        print("Tato operace neexistuje!!")
-    # else:   
-    #     print("Syntax ERROR")
+            print("Tato operace neexistuje!!")
+        if int(vysledek)==vysledek:
+            print(int(vysledek))
+        else:
+            print(vysledek)
+
+    except:
+        print("Syntax ERROR")
+        break
