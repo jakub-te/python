80a519085226c218e04c17ce99612ed11f88a952
unknown <hadmin@pcfeib207-77.msad.vsb.cz>
2025-11-27 16:52:51 +0100

    finalne hotovo, jdu na tkinter

--- pzthon.py
+++ pzthon.py
@@ -1,32 +1,33 @@
 
 #  NEUPRAVOVAT
+chyba = 0
 while True:
 
     vysledek=0
     true1=True
     true2=True
-    true3=True  
-    chyba = 0
+    true3=True
+
     while true1:
-        num1=input("prvni cislo: ").replace(" ", "")
+        num1=input("prvni cislo: ").replace(" ", "").replace(",", ".")
         try:
             if num1=="π" or num1=="pi":
                 num1=3.1415926535
             elif num1=="e":
-                num1=2.71828    
+                num1=2.7182818284
             else:
                 num1=float(num1)
             true1=False
         except:
             print("Syntax ERROR\nZkus to znovu!")
             chyba+=1
-            if chyba>=30:
+            if chyba>=10:
                 print("Radsi uz to nezkousej, STACILO!")
                 break
-    if chyba==30:
-        break 
+    if chyba>=10:
+        break
     while true2:
-        num2=input("druhe cislo: ").replace(" ", "")
+        num2=input("druhe cislo: ").replace(" ", "").replace(",", ".")
         try:
             if num2=="π" or num2=="pi":
                 num2=3.1415926535
@@ -38,37 +39,34 @@ while True:
         except:
             print("Syntax ERROR\nZkus to znovu!")
             chyba+=1
-            if chyba>=30:
+            if chyba>=10:
                 print("Radsi uz to nezkousej, STACILO!")
                 break
-    if chyba==30:
-        break            
+    if chyba>=10:
+        break
     while true3:
         oprt=input("operace: ").replace(" ", "")
-        try:
+        if oprt=="+" or oprt=="-" or oprt=="*":
 # TVUJ KOD:
-
             if oprt=="+":
                 vysledek=num1 + num2
             if oprt=="-":
                 vysledek=num1 - num2
             if oprt=="*":
                 vysledek=num1*num2
-            if oprt=="/":
-                if int(num2)==0:
-                    print("Jsi hloupý 0 nelze dělit!!!")
-                else:
-                    vysledek=num1 / num2
+
             if int(vysledek)==vysledek:
                 print(int(vysledek))
             if not int(vysledek)==vysledek:
                 print(vysledek)
-            True=False
-        except:
+            true3=False
+        elif oprt=="/" and not int(num2)==0:
+            vysledek=num1 / num2
+        else:
             print("Syntax ERROR\nZkus to znovu!")
             chyba+=1
-            if chyba>=30:
+            if chyba>=10:
                 print("Radsi uz to nezkousej, STACILO!")
                 break
-    if chyba==30:
-        break            
+    if chyba>=10:
+        break
